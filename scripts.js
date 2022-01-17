a2d6ebd14cc75b44cbd23ca51fedd500b0cb4731
julitacreations <90911265+julitacreations@users.noreply.github.com>
2022-01-17 13:36:36 +0200

    slick slider testimonial section added

--- scripts.js
+++ scripts.js
@@ -9,7 +9,22 @@ $( document ).ready(function() {
       // Animate on scroll - activate 
       AOS.init({
         offset: 80, // offset (in px) from the original trigger point
-        duration: 1100, // values from 0 to 3000, with step 50ms
+        duration: 900, // values from 0 to 3000, with step 50ms
+      });
+
+      // Slick-Slider animation
+      $('.testimonial-slider-container').slick({
+        autoplay: true, // Do we want it to autoplay? true or false
+        autoplaySpeed: 3000, // How long between each slide when auto-playing
+        speed: 500, // How fast is the transition in milliseconds
+        arrows: false, // Do you want to show arrows to trigger each slide
+        accessibility: true, // Enables keyboard tabbing and arrow key navigation
+        dots: true, // Enables the dots below to show how many slides
+        fade: false, // Changes the animate from slide to fade if true
+        infinite: true, // When true, means that it will scroll in a circle
+        pauseOnHover: true, // When true means the autoplay pauses when hovering
+        pauseOnDotsHover: true // Pauses the autoplay when hovering over the dots
+        
       });
 
 });
